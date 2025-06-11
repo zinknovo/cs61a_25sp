c40a0509c9e01ede18a10e32f6da88fc7da057dc
Z1nk <128562370+zinknovo@users.noreply.github.com>
2025-06-11 21:31:55 +0800

    Update lab01.py

--- lab/lab01.py
+++ lab/lab01.py
@@ -119,9 +119,4 @@ def double_eights(n):
     False
     """
     "*** YOUR CODE HERE ***"
-    count = 0
-    while n > 0:
-        if n % 10 == 8:
-            count += 1
-        n //= 10
-    return count == 2
+    return '88' in str(n)
